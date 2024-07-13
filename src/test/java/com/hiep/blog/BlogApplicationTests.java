1de11566632ddc63564c5b249250c895d1ed3bae
hiepdeptrai0908 <hiepdeptrai0908@gmail.com>
2024-07-13 13:35:21 +0700

    Spring Framework demo

---
+++ src/test/java/com/hiep/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hiep.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
